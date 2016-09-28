9e623091cdbe993285b5ba52835b16a5defcd3e1
Iastinov <iastinov@gmail.com>
2016-09-28 23:41:57 +0300

    not sure

--- handlers/submit.js
+++ handlers/submit.js
@@ -4,36 +4,51 @@ let imagesDB = []
 
 
 let addImages = (newItem) => {
-    let obj = {}
-    if (newItem) {
-        for (var item of newItem) {
-            item = item.toString().split('=')
-            obj[item[0]] = decodeURIComponent(item[1].split('+').join(' '))
-
-        }
+  let obj = {}
+  if (newItem) {
+    for (var item of newItem) {
+      item = item.toString().split('=')
+      console.log(item[0])
+      console.log(item[1])
+      if (item[1] !== '' && item[1] != null) {
+        obj[item[0]] = decodeURIComponent(item[1].split('+').join(' '))
         imagesDB.push(obj)
+      }
     }
-    console.log(imagesDB)
-}
 
+  }
+  console.log(imagesDB)
+}
+let getAllImages = (images) => {
+  let resultImages = ''
+  for (let obj of images) {
+    resultImages += `<img src="${obj.img_url}" alt="${obj.name}" ></br>`
+  }
+  return resultImages
+}
 module.exports = function (req, res, event) {
-    req.pathname = req.pathname || url.parse(req.url).pathname
-    if (req.method  === 'POST' && req.pathname === '/submit') {
-        var body = []
-        let obj = {}
-        req.on('data', function (chunk) {
-            body += chunk
-        }).on('end', function () {
-            body = JSON.stringify(body)
-            body = body.split('&')
-            addImages(body)
+  req.pathname = req.pathname || url.parse(req.url).pathname
+  if (req.method === 'POST' && req.pathname === '/submit') {
+    var body = []
+    req.on('data', function (chunk) {
+      body += chunk
+    }).on('end', function () {
+      body = JSON.stringify(body.replace('"', ''))
+      body = body.split('&')
+      addImages(body)
             // at this point, `body` has the entire request body stored in it as a string
-        });
-        var html = fs.readFileSync('index.html').toString();
-        res.writeHead(200, {
-        'Content-Type': "text/html",
-        })
-        res.write('<h2>Images was added</h2>')
-        res.end(html)
-    } else { return true }
+    })
+    //var html = fs.readFileSync('').toString()
+    
+    fs.readFile('./images.html', (err, data) => {
+      if (err) throw err
+      console.log(data)
+    })
+    let immgs = getAllImages(imagesDB)
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    res.write('<h2>Images was added</h2>')
+    res.end()
+  } else { return true }
 }
